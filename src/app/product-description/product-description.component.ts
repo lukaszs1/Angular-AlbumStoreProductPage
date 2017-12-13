7656463f830bf2ac7bc1a13ab309e0a4afef1c8b
lukaszs1 <lukas100x@gmail.com>
2017-12-13 21:57:08 +0100

    fix

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -13,7 +13,7 @@ export class ProductDescriptionComponent implements OnInit {
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => this.album = response)
+    this._productService.getAlbum(1).subscribe(response => this.album = response);
   }
 
 }
